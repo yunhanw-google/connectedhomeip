a62763e3bfb363255e6231ca31221526facf13b7
yunhanw <yunhanw@google.com>
2022-08-30 16:23:18 -0700

    experimentation

--- src/test_driver/linux-cirque/MobileDeviceTest.py
+++ src/test_driver/linux-cirque/MobileDeviceTest.py
@@ -48,7 +48,6 @@ DEVICE_CONFIG = {
         'capability': ['TrafficControl', 'Mount'],
         'rcp_mode': True,
         'docker_network': 'Ipv6',
-        'traffic_control': {'latencyMs': 25},
         "mount_pairs": [[CHIP_REPO, CHIP_REPO]],
     },
     'device1': {
@@ -57,7 +56,6 @@ DEVICE_CONFIG = {
         'capability': ['Thread', 'TrafficControl', 'Mount'],
         'rcp_mode': True,
         'docker_network': 'Ipv6',
-        'traffic_control': {'latencyMs': 25},
         "mount_pairs": [[CHIP_REPO, CHIP_REPO]],
     }
 }
